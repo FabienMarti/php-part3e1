25e539ab8a8c684bf25e33cb771ceffa90ec8497
FabienMarti <martiretti.fabien@gmail.com>
2020-06-06 15:53:55 +0200

    e

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+            $int = 0;
+            for ($int=0; $int <= 10; $int++) { 
+                echo($int);
+            }
+        ?>
+    </p>
+</body>
+</html>
